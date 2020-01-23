fa6f78b9f98f524d7e674acc3b8a5f078b4a1bec
Robert B Garcia <robgarcia552@gmail.com>
2020-01-22 23:33:21 -0500

    style welcome view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
-    def index
-        redirect_to profile_path if current_user
-    end
+  def index
+    redirect_to profile_path if current_user
+
+    @movie_samples = Movie.limit(3)
+  end
 end
