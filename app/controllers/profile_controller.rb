7c42dd69cc2ff1d3b427b00be442b22c56530c0d
Robert B Garcia <robgarcia552@gmail.com>
2020-01-24 21:27:50 -0500

    another edit to profile index for favorite

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,12 +2,12 @@ class ProfileController < ApplicationController
   before_action :authorize
 
   def index
-    @profile_movies ||= WatchedMovie.where(user_id: current_user.id)
+    @profile_movies ||= current_user.watched_movies
     @watched_movies ||= @profile_movies&.map do |m|
       Movie.find(m.movie_id)
     end
-    @favorite_movies ||= @profile_movies&.map do |m|
-      Movie.find(m.movie_id) if m.favorite
+    @favorite_movies ||= @profile_movies&.select{ |m| m.favorite }&.map do |m|
+      Movie.find(m.movie_id)
     end
   end
 end
